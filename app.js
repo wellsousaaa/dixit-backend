cd94969755ef5728399f0bef0141831e0bcfa494
Wendell Sousa <64435870+wellsousaaa@users.noreply.github.com>
2020-11-09 14:23:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ io.on("connection", (socket) => {
     });
 });
 
-serv.listen(process.env.PORT || 5000), () => {
+serv.listen((process.env.PORT || 5000), () => {
   console.log("Server on");
 });
 
