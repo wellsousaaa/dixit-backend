42879fe098fd8457df57f933f95a84faf6855ad3
Wendell Sousa <64435870+wellsousaaa@users.noreply.github.com>
2020-11-09 14:19:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://dixitfrontend.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", "*:*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
@@ -12,7 +12,7 @@ const serv = require("http").Server(app);
 const io = require("socket.io")(serv);
 const gameActions = require("./controllers/actions");
 
-io.origins(['https://dixitfrontend.herokuapp.com']);
+io.origins(["*:*"]);
 
 const cors = require("cors");
 app.use(cors());
